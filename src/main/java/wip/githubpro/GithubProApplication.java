2b28cef256847ccfc689d9c38c883c10f513fcc0
Wiktor Przetacznik <wiktorprzetacznik@outlook.com>
2024-07-20 13:36:56 +0200

    Initial setup

---
+++ src/main/java/wip/githubpro/GithubProApplication.java
@@ -0,0 +1,13 @@
+package wip.githubpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wip/githubpro/GithubProApplicationTests.java
@@ -0,0 +1,13 @@
+package wip.githubpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
