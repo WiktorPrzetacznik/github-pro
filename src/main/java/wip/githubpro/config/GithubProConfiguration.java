41ea05633a509d2a7b0ff22606bc5101a456aff3
Wiktor Przetacznik <wiktorprzetacznik@outlook.com>
2024-07-21 15:15:55 +0200

    Implement api pagination support

--- src/main/java/wip/githubpro/config/GithubProConfiguration.java
+++ src/main/java/wip/githubpro/config/GithubProConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilderFactory;
+
+import java.util.regex.Pattern;
 
 @Configuration
 public class GithubProConfiguration {
@@ -11,9 +15,18 @@ public class GithubProConfiguration {
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
-                .baseUrl("https://api.github.com")
                 .defaultHeader(HttpHeaders.ACCEPT, "application/json")
                 .build();
+    }
+
+    @Bean
+    public Pattern githubNextPageHeaderPattern() {
+        return Pattern.compile("<(.+)>; rel=\"next\"");
+    }
+
+    @Bean
+    public UriBuilderFactory githubUriBuilderFactory() {
+        return new DefaultUriBuilderFactory("https://api.github.com");
     }
 
 }
--- src/main/java/wip/githubpro/service/DefaultRepositoryService.java
+++ src/main/java/wip/githubpro/service/DefaultRepositoryService.java
@@ -2,21 +2,32 @@ package wip.githubpro.service;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriBuilderFactory;
 import wip.githubpro.model.NotForkRepoData;
 import wip.githubpro.model.repository.Branch;
 import wip.githubpro.model.repository.Repository;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class DefaultRepositoryService implements RepositoryService {
 
     private final RestClient restClient;
+    private final Pattern githubNextPageHeaderPattern;
+    private final UriBuilderFactory githubUriBuilderFactory;
 
-    public DefaultRepositoryService(RestClient restClient) {
+    public DefaultRepositoryService(RestClient restClient, Pattern githubNextPageHeaderPattern, UriBuilderFactory githubUriBuilderFactory) {
         this.restClient = restClient;
+        this.githubNextPageHeaderPattern = githubNextPageHeaderPattern;
+        this.githubUriBuilderFactory = githubUriBuilderFactory;
     }
 
     @Override
@@ -32,17 +43,51 @@ public class DefaultRepositoryService implements RepositoryService {
     }
 
     private List<Repository> getUserRepositories(String username) {
-        return restClient.get()
-                .uri(b -> b.path("/users/{username}/repos").build(username))
-                .retrieve()
-                .body(new ParameterizedTypeReference<>(){});
+        return getData(
+                githubUriBuilderFactory
+                        .builder()
+                        .path("/users/{username}/repos")
+                        .build(username),
+                new ParameterizedTypeReference<>(){}
+        );
     }
 
     private List<Branch> getRepositoryBranches(String owner, String repo) {
-        return restClient.get()
-                .uri(b -> b.path("/repos/{owner}/{repo}/branches").build(owner, repo))
+        return getData(
+                githubUriBuilderFactory
+                        .builder()
+                        .path("/repos/{owner}/{repo}/branches")
+                        .build(owner, repo),
+                new ParameterizedTypeReference<>(){}
+        );
+    }
+
+    private <T> List<T> getData(URI uri, ParameterizedTypeReference<List<T>> ptr) {
+        HttpEntity<List<T>> responseEntity = restClient.get()
+                .uri(uri)
                 .retrieve()
-                .body(new ParameterizedTypeReference<>(){});
+                .toEntity(ptr);
+        List<T> data = responseEntity.getBody();
+        HttpHeaders headers = responseEntity.getHeaders();
+        if (headers.containsKey("link")) {
+            getNextPaginationURI(headers.get("link"))
+                    .ifPresent(u -> data.addAll(getData(u, ptr)));
+        }
+        return data;
+    }
+
+    private Optional<URI> getNextPaginationURI(List<String> linkHeaders) {
+        if (!linkHeaders.isEmpty()) {
+            for (String header : linkHeaders) {
+                for (String processedHeader : header.split(",")) {
+                    Matcher matcher = githubNextPageHeaderPattern.matcher(processedHeader);
+                    if (matcher.find()) {
+                        return Optional.of(URI.create(matcher.group(1)));
+                    }
+                }
+            }
+        }
+        return Optional.empty();
     }
 
 }
